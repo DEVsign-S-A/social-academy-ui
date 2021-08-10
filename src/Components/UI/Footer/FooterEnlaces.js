10414e63159576f3f7974dd7fe7b4354b4b576a9
Jeisson Gonzalez <jeiglz13@gmail.com>
2021-08-10 11:01:16 -0600

    footer responsive v1

--- src/Components/UI/Footer/FooterEnlaces.js
+++ src/Components/UI/Footer/FooterEnlaces.js
@@ -4,10 +4,8 @@ import { NavLink } from 'react-router-dom';
 export const FooterEnlaces = ({titulo, link}) => {
     return (
         <div className ="Enlace">
-            <NavLink exact to = {link}>
-                <a className = "pointer">
+            <NavLink exact to = {link} className = "pointer">
                     {titulo}
-                </a>
             </NavLink>
         </div>
     )
