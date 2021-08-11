dac00dd837717d14426ccffe32dd1c1dd2253105
Jeisson Gonzalez <jeiglz13@gmail.com>
2021-08-10 18:31:00 -0600

    fix links social media

--- src/Components/UI/Footer/FooterSocialItem.js
+++ src/Components/UI/Footer/FooterSocialItem.js
@@ -3,7 +3,11 @@ import React from 'react'
 export const FooterSocialItem = ({path, enlace}) => {
     return (
         <div className ="pointer media">
-            <a href = {enlace}><i className={path}></i></a>
+            <a href = {enlace} 
+                target="_blank" 
+                rel="noopener noreferrer">
+                    <i className={path}></i>
+            </a>
         </div>
     )
 }
