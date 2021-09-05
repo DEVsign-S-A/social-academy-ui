066721909c011aa0adeae07971c8c09f51e58e2c
Kevin Garcia <gkevinyamil@gmail.com>
2021-09-05 16:18:09 -0600

    Creating login

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -9,7 +9,7 @@ import CoursesScreen from '../Views/CoursesScreen';
 import ForumScreen from '../Views/ForumScreen';
 import HomeScreen from '../Views/HomeScreen';
 import InternshipsScreen from '../Views/InternshipsScreen';
-import LoginScreen from '../Views/LoginScreen';
+import {LoginView} from '../Components/UI_Login/LoginView';
 import PageNoFound from '../Views/PageNoFound';
 import ProfileScreen from '../Views/ProfileScreen';
 import ResourcesScreen from '../Views/ResourcesScreen';
@@ -28,7 +28,7 @@ export const AppRouter = () => {
                     <Route exact path='/Cursos' component={CoursesScreen}/>
                     <Route exact path='/Recursos' component={ResourcesScreen}/>
                     <Route exact path='/About' component={AboutScreen}/>
-                    <Route exact path='/Login' component={LoginScreen}/>
+                    <Route exact path='/Login' component={LoginView}/>
                     <Route exact path='/PageNoFound' component={PageNoFound}/>
                     <Route exact path='/Profile/:userId' component={ProfileScreen}/>
                 </Switch>
