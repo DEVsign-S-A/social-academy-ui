ca42640c43649a5cba7e4221ecbdd4e3d6d1a607
Jeisson Gonzalez <jeiglz13@gmail.com>
2021-10-09 11:08:15 -0600

    firebase version 8.6

--- src/Components/UI_UserProfile/UserInfo.js
+++ src/Components/UI_UserProfile/UserInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import moment from 'moment';
+import moment from 'moment';
 import { RedSocial } from './UserProfileComponents/RedSocial';
 
 
@@ -14,7 +14,7 @@ export const UserInfo = ({nombreUsuario,
     socialMedia, 
     curriculumLink}) => {
     
-    // const annos = moment().diff(fechaNacimiento, 'years', false);
+    const annos = moment().diff(fechaNacimiento, 'years', false);
 
     const redesSociales = [
         
@@ -34,7 +34,7 @@ export const UserInfo = ({nombreUsuario,
                     <h3>{carrera}</h3>
             
                     <div className = "UserInformation">
-                        {/* <h4><b>Edad:</b> {annos} años </h4> */}
+                        <h4><b>Edad:</b> {annos} años </h4>
                         <h4><b>Ciudad:</b> {direccion.ciudad}, {direccion.departamento}, {direccion.pais} </h4>
                         <h4><b>Telefono:</b> {telefono}</h4>
                         <h4><b>Correo:</b> {correo}</h4>
